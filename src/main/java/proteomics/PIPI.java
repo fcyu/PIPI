afb8fffbd89225a0e8e25c168ace650a7d72ae7a
Fengchao <fyuab@connect.ust.hk>
2016-09-27 10:59:06 +0800

    New release.

--- src/main/java/proteomics/PIPI.java
+++ src/main/java/proteomics/PIPI.java
@@ -26,7 +26,7 @@ public class PIPI {
 
     private static final Logger logger = LoggerFactory.getLogger(PIPI.class);
     private static final float C13Diff = 1.00335483f;
-    private static final String versionStr = "1.2.2";
+    private static final String versionStr = "1.2.3";
 
     public static final boolean DEV = false;
 
