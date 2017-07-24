0fea4043dc46c00f598a55ed9df25c2dc2862554
Fengchao <fyuab@connect.ust.hk>
2017-07-24 18:54:55 +0800

    New release.

--- src/main/java/proteomics/PIPI.java
+++ src/main/java/proteomics/PIPI.java
@@ -25,7 +25,7 @@ import java.util.concurrent.TimeUnit;
 public class PIPI {
 
     private static final Logger logger = LoggerFactory.getLogger(PIPI.class);
-    public static final String versionStr = "1.3.0-dev";
+    public static final String versionStr = "1.3.0";
     public static final boolean useXcorr = true;
 
     public static final boolean DEV = false;
