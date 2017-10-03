52df48449b712b389ceaeffc56763077389fcbe6
Fengchao <fyuab@connect.ust.hk>
2017-10-03 16:21:27 +0800

    Change the PTM mass decimal point to three.

--- src/main/java/proteomics/Types/VarModParam.java
+++ src/main/java/proteomics/Types/VarModParam.java
@@ -15,7 +15,7 @@ public class VarModParam {
         this.aa = aa;
         this.priority = priority;
 
-        toString = Math.round(mass * 100) + "@" + aa;
+        toString = Math.round(mass * 1000) + "@" + aa;
         hashCode = toString.hashCode();
     }
 
