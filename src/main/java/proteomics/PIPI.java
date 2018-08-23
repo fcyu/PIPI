ff600add64150e4533deab7e80ba38c521e646fc
Fengchao <fyuab@connect.ust.hk>
2018-08-23 21:58:38 +0800

    New release.

--- src/main/java/proteomics/PIPI.java
+++ src/main/java/proteomics/PIPI.java
@@ -27,7 +27,7 @@ import java.util.concurrent.locks.ReentrantLock;
 public class PIPI {
 
     private static final Logger logger = LoggerFactory.getLogger(PIPI.class);
-    public static final String versionStr = "1.4.5";
+    public static final String versionStr = "1.4.6";
     static final boolean useXcorr = true;
 
     public static final int[] debugScanNumArray = new int[]{};
